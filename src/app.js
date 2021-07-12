634592fd56672958586578f4bd35b51598426d07
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-12 14:50:35 -0300

    route to create new itens

--- src/app.js
+++ src/app.js
@@ -7,4 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/list', async (req,res)=>{
+    try{
+        
+        await connection.query(`
+        INSERT 
+        INTO list (text) 
+        VALUES ($1)
+        `,[req.body.text])
+        res.sendStatus(201)
+
+    }
+    catch (e){
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
 export default app;
