c75eccba60327c7dcdc7203c8605160fe3bfb582
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-12 15:18:43 -0300

    test for get rout

--- tests/list.test.js
+++ tests/list.test.js
@@ -3,20 +3,28 @@ import app from "../src/app";
 import connection from "../src/database";
 
 
-beforeEach(async ()=>{
-    await connection.query(`DELETE FROM list `)
-})
-
 afterAll(()=>{
     connection.end();
 })
 
 describe("POST /list", ()=>{
-    beforeEach(async ()=>{
-        await connection.query(`INSERT INTO list (text) VALUES ('batata')`)
+    beforeAll(async ()=>{
+        await connection.query(`DELETE FROM list `)
+        await connection.query(`INSERT INTO list (text) VALUES ('batata'),('matte'),('banana'),('abacaxi')`)
     })
     it('return status 201 from valid text', async ()=>{
         const newItem ={text:'abacate'}
         const result = await supertest(app).post('/list').send(newItem)
+        expect(result.status).toEqual(201);
+    })
+})
+describe("POST /list", ()=>{
+    beforeAll(async ()=>{
+        await connection.query(`DELETE FROM list `)
+        await connection.query(`INSERT INTO list (text) VALUES ('batata'),('matte'),('banana'),('abacaxi')`)
+    })
+    it('return an array with 4 itens from valid URL', async ()=>{
+        const result = await supertest(app).get('/list')
+        expect(result.body.length).toEqual(4);
     })
 })
